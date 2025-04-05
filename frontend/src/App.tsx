80aaad24ff8c05e97add6194183df00602139ae7
Mark Quach <markquach@student.unsw.edu.au>
2025-04-06 00:51:13 +1100

    Added navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import Predict from './components/pages/Predict'
 import Band6 from './components/pages/Band6'
+import Navbar from './components/pages/Navbar'
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <div className="pt-20 px-4">
+    <Navbar />
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/"/>
         <Route path="/predict" element={<Predict />} />
         <Route path="/band6" element={<Band6 />} />
       </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/components/pages/Navbar.tsx
@@ -0,0 +1,39 @@
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+  } from "@/components/ui/navigation-menu"
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+      <div className="fixed top-0 left-0 w-full bg-white border-b shadow p-4">
+        <NavigationMenu>
+          <NavigationMenuList className="flex space-x-4">
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <Link to="/" className="!text-black">Home</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <Link to="/predict" className="!text-black">Predict</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <Link to="/band6" className="!text-black">Band 6</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    );
+  }
+
+export default Navbar
