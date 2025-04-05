db894e2e96f38f3d84b48727f866fcea0cfce614
Mark Quach <markquach@student.unsw.edu.au>
2025-04-06 00:58:11 +1100

    Fixed deployment bug again

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,20 @@
-import './App.css'
-import Predict from './components/pages/Predict'
-import Band6 from './components/pages/Band6'
-import Navbar from './components/pages/Navbar'
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Predict from "./components/pages/Predict";
+import Band6 from "./components/pages/Band6";
+import Navbar from "./components/pages/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="pt-20 px-4">
-    <Navbar />
+      <Navbar />
       <Routes>
         <Route path="/"/>
         <Route path="/predict" element={<Predict />} />
         <Route path="/band6" element={<Band6 />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
